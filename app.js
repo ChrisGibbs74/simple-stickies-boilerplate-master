6524957b63c8754fc03a47a8d4d5e0cc6cbdf5a0
ChrisGibbs74 <crgibbs@wisc.edu>
2026-02-23 20:52:28 +0100

    Add and delete notes

--- app.js
+++ app.js
@@ -5,10 +5,27 @@ Vue.createApp({
     };
   },
 
-  mounted() {
-    this.stickies = [
-      { id: 1, text: "Test note 1" },
-      { id: 2, text: "Another note" }
-    ];
+  methods: {
+    addStickie() {
+      const id =
+        (typeof crypto !== "undefined" && crypto.randomUUID)
+          ? crypto.randomUUID()
+          : String(Date.now() + Math.random());
+
+      this.stickies.push({ id, text: "" });
+    },
+
+    deleteStickie(id) {
+      this.stickies = this.stickies.filter((s) => s.id !== id);
+    },
+
+    clearAll() {
+      // Commit 3: you can keep this simple for now
+      this.stickies = [];
+    },
+
+    charCount(text) {
+      return (text ?? "").length;
+    }
   }
 }).mount("#app");
